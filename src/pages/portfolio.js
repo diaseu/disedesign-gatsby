a38f6d83e2ff512f1b63838f1720364cbde54773
Dia Seu <diana.seung@gmail.com>
2021-10-19 19:01:00 -0700

    portfolio link and order

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -17,16 +17,26 @@ const Portfolio = () => {
   const projects = [
     {
       id: 0,
+      name: "Dise Design v2",
+      blurb: "Personal Portfolio Site v2 for Dia Seung",
+      description: "Dise Design v2 was a redesign of my personal portfolio site. As I worked on the first version, I realized I wasn't too thrilled with that implementation I wanted to explore using new technology, especially Gatsby and SASS, to improve my skills.  showcasing recent projects I've worked on, as well as info about me including how to reach me.",
+      url: "https://www.disedesign.com",
+      git: "https://github.com/diaseu/disedesign-gatsby",
+      img: "https://i.imgur.com/ClXP6wG.png",
+      technology: ["Javascript", "React.JS", "Node.JS", "Ant Design"]
+    },
+    {
+      id: 1,
       name: "Dise Design v1",
       blurb: "Personal Portfolio Site v1 for Dia Seung",
       description: "Dise Design v1 was my version of my personal portfolio site that showcased recent projects I've worked on, as well as info about me. This site was an exploration of the Ant Design styling library, which was a break away from Material UI. ",
-      url: "https://www.disedesign.com",
+      url: "https://silly-brattain-2e84c2.netlify.app/",
       git: "https://github.com/diaseu/disedesign-netlify",
       img: "https://i.imgur.com/xwtIYfG.png",
       technology: ["Javascript", "React.JS", "Node.JS", "Ant Design"]
     },
     {
-      id: 1,
+      id: 2,
       name: "Zap: Bug Tracker",
       blurb: "Social bug tracking app, with the ability to crowd-source solutions to issues and questions",
       description: "Not everything needs to be a revolutionary new idea. Sometimes we just want to take something that already exists and simply make it better.  We wanted to make a bug tracking app that innovates on currently existing forms such as Trello or Jira, and give it a social media flavor.  Zap App gives users the ability to create projects and organize tasks, bugs and issues, and then reach out to the community to crowdfund solutions and answers.",
@@ -36,7 +46,7 @@ const Portfolio = () => {
       technology: ["Javascript", "MongoDB", "Express", "React.JS", "Node.JS", "Material-UI"]
     },
     {
-      id: 2,
+      id: 3,
       name: "Study Blog",
       blurb: "Social blog app where users can post, comment and vote on student-created content",
       description: "Study Blog is a student-centered forum website for students to share notes, curated and promoted through voting. Users are able to create rich-text posts chasing any notes and articles related to a specific topic, which are sorted into topic categories. Users can register by creating a username and a password and entering their email address. All passwords are encrypted using passport and jsonwebtoken modules. Users can upvote/downvote or comment on other users' posts. All the user, post, and comment data is saved in an SQL database using the JAWSDB add-on on Heroku. This application allows users to post study material for the following topics:  HTML, Javascript, Node.JS, SQL",
@@ -46,7 +56,7 @@ const Portfolio = () => {
       technology: ["Javascript", "MySQL2", "Express", "React.JS", "Node.JS", "Passport", "JSON Web Token", "Sequelize"]
     },
     {
-      id: 3,
+      id: 4,
       name: "Beleaf Co.",
       blurb: "Online boutique shop specializing in rare tropical plants",
       description: "Beleaf Co. is a online boutique shop specializing in sales of rare tropical plants and various related accessories. This started as a personal passion in collecting rare tropical houseplants, which eventually led to sharing the plant love with the local community. COMING SOON ",
@@ -55,16 +65,7 @@ const Portfolio = () => {
       img: "https://i.imgur.com/nJf59GQ.png",
       technology: ["Javascript", "MongoDB", "Express", "React.JS", "Node.JS", "Commerce.JS", "Material-UI"]
     },
-    {
-      id: 4,
-      name: "Dise Design v2",
-      blurb: "Personal Portfolio Site v2 for Dia Seung",
-      description: "Dise Design v2 was a redesign of my personal portfolio site. As I worked on the first version, I realized I wasn't too thrilled with that implementation I wanted to explore using new technology, especially Gatsby and SASS, to improve my skills.  showcasing recent projects I've worked on, as well as info about me including how to reach me.",
-      url: "https://www.disedesign.com",
-      git: "https://github.com/diaseu/disedesign-netlify",
-      img: "https://i.imgur.com/ClXP6wG.png",
-      technology: ["Javascript", "React.JS", "Node.JS", "Ant Design"]
-    },
+    
   ]
 
   // function openPane(e) {
